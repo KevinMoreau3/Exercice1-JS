556832e23904f8799de7994efca1557417207c18
Moreau Kévin <moreau_kevin@yahoo.fr>
2013-10-24 21:07:47 +0200

    Update exercice1.js

--- exercice1.js
+++ exercice1.js
@@ -1,36 +1,30 @@
-function adder()
-{
-	var args = Array.prototype.slice.call(arguments);
-	
-	return function(v)
-	{
-		var result=0;
-		args.forEach(function(val)
-		{
-			result = result + val(v);
-			});
-		return result;
-		
-		}
-	
-	}
-}
-function mult(v)
-{
-	return function(e)
-        {
-		return v*e;
-		}
-	}
+function adder() {
+    var args = Array.prototype.slice.call(arguments);
 
-function sub(a)
-{
-	return function(b)
-	{
-		return a-b;
-		}
-	
-	}
+    return function (v) {
+        var result = 0;
+        args.forEach(function (val) {
+            result = result + val(v);
+        });
+        return result;
+
+    }
+
+}
+}
+
+function mult(v) {
+    return function (e) {
+        return v * e;
+    }
+}
+
+function sub(a) {
+    return function (b) {
+        return a - b;
+    }
+
+}
 
 console.log(adder(mult(2), mult(4))(2));
 console.log(sub(0)(0));
@@ -42,4 +36,3 @@ console.log(adder()(1));
 console.log(mult(2)(1));
 console.log(adder(mult(2), mult(2))(1));
 console.log(adder(mult(2), mult(2), mult(2))(1));
-console.log(adder(mult(2), sub(2), mult(2))(1));
